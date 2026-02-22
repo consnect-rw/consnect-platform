f851b9210067bbc4f3d23aa5b79368fffd3363d1
diibro <brother.dushime@gmail.com>
2026-02-22 10:14:40 +0200

    updates-auth-layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,10 +1,10 @@
 import { AuthProvider } from "@/context/AuthContext";
+import AuthWrapper from "@/context/AuthWrapper";
 import { getSessionUser } from "@/server/auth/user";
 import { ReactNode } from "react";
 
 export default async function AuthLayout ({children}:{children: ReactNode}) {
-     const {user} = await getSessionUser();
      return (
-          <AuthProvider authUser={user}>{children}</AuthProvider>
+          <AuthWrapper>{children}</AuthWrapper>
      )
 }
